c5e7550a8ed221f43c28016ceeeb8b1c84d91d3c
Dat Adithya <naruita201@gmail.com>
2020-04-23 16:05:41 +0530

    Started daytwo.py, haven't made big changes yet. Trying to import successfully.

---
+++ days/daytwo.py
@@ -0,0 +1,11 @@
+import pickle
+import time
+import dayone
+#Need to add the alert of having the day two appear
+if(dayone.stay_at_hotel){
+    print("You wake up feeling groggy but very comfortable, after getting up from the bed,\n you admire the view from the room"
+    + " and go down to the dining section to have the first meal of the day."
+    +"\nYou glance around and see the rush of the busy folk. "
+    +"\nSeems like a drag.")
+    time.sleep(6)
+}
